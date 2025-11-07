7c97fd5025111198713e1909bae9f95f5c90af01
Samnangalex06 <samnangalextt@gmail.com>
2025-11-07 13:46:55 +0700

    Update components and image paths

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,134 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+<template>
+  <div class="full_display">
+    <div class="category-display">
+      <Category
+        v-for="category in categories"
+        :key="category.categoryName"
+        :image="category.image"
+        :categoryName="category.categoryName"
+        :num="category.num"
+        :bgColor="category.bgColor"
+      />
+    </div>
+
+    <div class="banner_display">
+      <Banner
+        v-for="banner in banners"
+        :key="banner.title"
+        :title="banner.title"
+        :bgColor="banner.bgColor"
+        :bgImage="banner.bannerImage"
+        :ButtonbgColor="banner.ButtonbgColor"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Category from './components/Category.vue'
+import Banner from './components/Promotion.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Category,
+    Banner,
+  },
+
+  data() {
+    return {
+      categories: [
+        { categoryName: 'Cake & Milk', num: 14, bgColor: '#F2FCE4', image: '' },
+        { categoryName: 'Peach', num: 17, bgColor: '#FFFCEB', image: '/images/category/peach.png' },
+        {
+          categoryName: 'Organic Kiwi',
+          num: 21,
+          bgColor: '#ECFFEC',
+          image: '/images/category/kiwi.png',
+        },
+        {
+          categoryName: 'Red Apple',
+          num: 68,
+          bgColor: '#FEEFEA',
+          image: '/images/category/apple.png',
+        },
+        { categoryName: 'Snack', num: 34, bgColor: '#FFF3EB', image: '/images/category/snack.png' },
+        {
+          categoryName: 'Black plum',
+          num: 25,
+          bgColor: '#FFF3FF',
+          image: '/images/category/blackplum.png',
+        },
+        {
+          categoryName: 'Vegetables',
+          num: 65,
+          bgColor: '#F2FCE4',
+          image: '/images/category/vegetables.png',
+        },
+        {
+          categoryName: 'Headphone',
+          num: 33,
+          bgColor: '#FFFCEB',
+          image: '/images/category/headphone.png',
+        },
+        {
+          categoryName: 'Cake & Milk',
+          num: 54,
+          bgColor: '#F2FCE4',
+          image: '/images/category/cakemike.png',
+        },
+        {
+          categoryName: 'Orange',
+          num: 63,
+          bgColor: '#FFF3FF',
+          image: '/images/category/orange.png',
+        },
+      ],
+
+      banners: [
+        {
+          title: 'Everyday Fresh & Clean with Our Products',
+          bgColor: '#F0E8D5',
+          bannerImage: '/images/banner/onion.jpg',
+          ButtonbgColor: '#3BB77E',
+        },
+        {
+          title: 'Make your Breakfast Healthy and Easy',
+          bgColor: '#F3E8E8',
+          bannerImage: '/images/banner/juice.png',
+          ButtonbgColor: '#3BB77E',
+        },
+        {
+          title: 'The best Organic Products Online',
+          bgColor: '#E7EAF3',
+          bannerImage: '/images/banner/basketofVegetables.jpg',
+          ButtonbgColor: '#FDC040',
+        },
+      ],
+    }
+  },
+}
 </script>
 
-<template>
-  <h1>This is my first Vue</h1>
-</template>
+<style scoped>
+.full_display {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+
+.category-display {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 20px;
+}
+
+.banner_display {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 75px;
+  gap: 20px;
+}
+</style>
---
+++ src/components/Category.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="card" :style="{ backgroundColor : bgColor}">
+      <img id="category-image" :src="image" alt="">
+      <p id="category-name">{{ categoryName }}</p>
+      <p id="category-num">{{ num }} items</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Category',
+  props: {
+      categoryName: {
+          type: String,
+          required: true
+      },
+      num: {
+          type: Number,
+          required: true
+      },
+
+      bgColor: {
+          type: String,
+          default: '#ffffff'
+      },
+      image: {
+          type: String,
+          default: ''
+      }
+  }
+}
+</script>
+
+<style scoped>
+  .card {
+      width: 100%;
+      max-width: 137px;
+      height: 177px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      border-radius: 10px;
+  }
+  #category-image {
+      width: 120px;
+      height: 120px;
+  }
+  #category-name {
+      font-family: 'Quicksand', sans-serif;
+      font-size: 16px;
+      color: rgba(37, 61, 78, 1);
+  }
+
+  #category-num {
+      font-family: 'Lato', sans-serif;
+      font-size: 12px;
+      color: rgba(182, 182, 182, 1);
+  }
+</style>
